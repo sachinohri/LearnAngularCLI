c4835fc4ff12668ac104cf98dd96fd6659a1c43c
Sachin <ohri.sachin@gmail.com>
2017-07-14 00:01:00 +0530

    Step 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule

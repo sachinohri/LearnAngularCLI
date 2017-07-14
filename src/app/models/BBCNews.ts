23e2d17096c7ad1812fdb9e8f474bc7451bf2b09
Sachin <ohri.sachin@gmail.com>
2017-07-15 00:08:54 +0530

    Step 1

---
+++ src/app/models/BBCNews.ts
@@ -0,0 +1,6 @@
+export class BBCNews{
+    author:string;
+    title:string;
+    description:string;
+    url:string;
+}
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BBCNews} from '../models/BBCNews';
 
 @Component({
   selector: 'app-stories',
@@ -8,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
 
 export class StoriesComponent implements OnInit {
   items: number[];
-
+  stories: BBCNews[];
   constructor() {
-    this.items = Array(30);
+    this.items = Array(10);
   }
 
-  ngOnInit() {
+  fetchStories(){
+      this.stories= new Array();
+      for(let i=1;i<11;i++){
+        let news: BBCNews = new BBCNews;
+        news.author = "BBC Sports";
+        news.title = "Title " + i;
+        news.url = "http://typescriptlang.org"
+        news.description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        this.stories.push(news);
+      }
   }
+  ngOnInit(){
+    this.fetchStories();
+  }
+  
 }

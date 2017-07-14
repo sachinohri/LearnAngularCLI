339fef3fc9f87e5ff142691ce94c35e083b619fa
Sachin <ohri.sachin@gmail.com>
2017-07-15 00:11:33 +0530

    minor

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -14,19 +14,20 @@ export class StoriesComponent implements OnInit {
     this.items = Array(10);
   }
 
-  fetchStories(){
-      this.stories= new Array();
+  fetchStories(): BBCNews[]{
+      let BBCnews: BBCNews[]= new Array();
       for(let i=1;i<11;i++){
         let news: BBCNews = new BBCNews;
         news.author = "BBC Sports";
         news.title = "Title " + i;
         news.url = "http://typescriptlang.org"
         news.description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        this.stories.push(news);
+        BBCnews.push(news);
       }
+      return BBCnews
   }
   ngOnInit(){
-    this.fetchStories();
+    this.stories = this.fetchStories();
   }
   
 }

3c245e2c18b1bce5a56e8dc4d3ff36625aefee0d
Sachin <ohri.sachin@gmail.com>
2017-07-23 00:38:21 +0530

    changes

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
